579b59de432e4cb1e98d524c0ce7c8678eb0c03f
Victoria Drake <24644237+victoriadrake@users.noreply.github.com>
2024-12-31 13:45:13 -0600

    Update action.sh

--- action.sh
+++ action.sh
@@ -18,9 +18,10 @@ echo '🔧 Install tools'
 npm init -y && npm install -y postcss postcss-cli autoprefixer
 
 echo '🤵 Install Hugo'
+HUGO_VERSION=$(curl -s https://api.github.com/repos/gohugoio/hugo/releases/latest | jq -r '.tag_name')
 mkdir tmp/ && cd tmp/
-curl -sSL $(curl -s https://api.github.com/repos/gohugoio/hugo/releases/latest | grep "browser_download_url.*\hugo_extended.*\_Linux-64bit.tar.gz" | rev | cut -d ' ' -f 1 | rev | tr -d '"') | tar -xvzf-
-mv hugo /usr/local/bin/
+curl -sSL https://github.com/gohugoio/hugo/releases/download/${HUGO_VERSION}/hugo_extended_${HUGO_VERSION: -7}_Linux-64bit.tar.gz | tar -xvzf-
+sudo mv hugo /usr/local/bin/
 cd .. && rm -rf tmp/
 cd ${GITHUB_WORKSPACE}
 hugo version || exit 1
